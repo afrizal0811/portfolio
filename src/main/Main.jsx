21b8454d2c7b23daf82968c04e0042cfa05b44cb
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-01-29 17:52:16 +0700

    add link

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,70 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { StyledArrow } from './StyledComponents'
+
 const Main = ({ className }) => {
+  const list = (text) => (
+    <Link
+      to={`/${text}`}
+      className='arrow'
+    >
+      <StyledArrow
+        src='logo-yellow.png'
+        className='sign'
+      />
+      {text}
+    </Link>
+  )
   return (
     <div className={className}>
       <div className='wrapper'>
         <ol>
-          <li>
-            <a
-              className='arrow'
-              href='#as'
-            >
-              <StyledArrow
-                src='logo-yellow.png'
-                className='sign'
-              />
-              Tetris
-            </a>
-          </li>
-          <li>
-            <a
-              className='arrow'
-              href='#as'
-            >
-              <StyledArrow
-                src='logo-yellow.png'
-                className='sign'
-              />
-              Crazy Taxi
-            </a>
-          </li>
-          <li>
-            <a
-              className='arrow'
-              href='#as'
-            >
-              <StyledArrow
-                src='logo-yellow.png'
-                className='sign'
-              />
-              Super Mario
-            </a>
-          </li>
-          <li>
-            <a
-              className='arrow'
-              href='#as'
-            >
-              <StyledArrow
-                src='logo-yellow.png'
-                className='sign'
-              />
-              Mortal Combat
-            </a>
-          </li>
-          <li>
-            <a
-              className='arrow'
-              href='#as'
-            >
-              <StyledArrow
-                src='logo-yellow.png'
-                className='sign'
-              />
-              Street Fighter
-            </a>
-          </li>
+          {/* <li>{list('home')}</li> */}
+          <li>{list('about')}</li>
+          <li>{list('projects')}</li>
+          <li>{list('contact')}</li>
         </ol>
       </div>
     </div>
