8e5385895b0ad4190c0cb186423f091c1d30f0ff
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-02-17 13:07:14 +0700

    edit typing speed

--- src/components/text-box/TextBox.jsx
+++ src/components/text-box/TextBox.jsx
@@ -5,7 +5,7 @@ import { useTypingEffect } from '../../utilities/typingEffect'
 const TextBox = (props) => {
   const { isInView, option, setIsFinished } = props
   const [isSeen, setIsSeen] = useState(false)
-  const delayTime = isInView ? (isSeen ? 20 : 2000) : 20
+  const delayTime = isInView ? (isSeen ? 30 : 2000) : 30
   const textDisplay = useTypingEffect(
     answerTextList[option],
     delayTime,
