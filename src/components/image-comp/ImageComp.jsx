2bdd1e7a9b9c4b3b2c0c448395846eb0f6d54412
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-02-18 23:00:42 +0700

    fix image comp

--- src/components/image-comp/ImageComp.jsx
+++ src/components/image-comp/ImageComp.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const ImageComp = (props) => {
-  const { className, src, alt } = props
+  const { className, src, alt, height } = props
 
   return (
     <img
       alt={alt}
       className={className}
       src={src}
+      height={height}
       draggable='false'
       loading='lazy'
     />
