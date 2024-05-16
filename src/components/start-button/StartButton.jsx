58c51388d9b8580ec4a0d3625dc6511019e7185a
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-05-16 09:47:21 +0700

    add func press keyboard

--- src/components/start-button/StartButton.jsx
+++ src/components/start-button/StartButton.jsx
@@ -13,10 +13,28 @@ const StartButton = (props) => {
     setStarted(true)
   }
 
+  const handleKeyDown = (e) => {
+    const spaceCode = e.keyCode === 32
+    const enterCode = e.keyCode === 13
+    
+    if (spaceCode || enterCode) {
+      setIsClicked(true)
+      setStarted(true)
+    }
+  }
+
   useEffect(() => {
     setTimeout(() => {
       if (isClicked) myContainer.current.innerText = ''
     }, 500)
+
+    document.addEventListener('keydown', handleKeyDown)
+
+    // Don't forget to clean up
+    return function cleanup() {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isClicked])
 
   return (
@@ -26,7 +44,8 @@ const StartButton = (props) => {
     >
       <button
         type='button'
-        onClick={() => handleOnClick()}
+        onClick={handleOnClick}
+        onKeyDown={(e) => handleKeyDown(e)}
       >
         <h1 ref={myContainer}>Press Start</h1>
       </button>
