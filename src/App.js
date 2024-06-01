3569ac580186471fa8e44f53b47eafcb781192ae
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-06-01 13:20:03 +0700

    test 100vh

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import { ToastContainer } from 'react-custom-alert'
 import 'react-custom-alert/dist/index.css'
-import Div100vh from 'react-div-100vh'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 import RouteHandler from './RouteHandler'
 
 function App() {
   return (
-    <Div100vh className='App'>
+    <div className='App'>
       <ToastContainer floatingTime={1000} />
       <Router>
         <RouteHandler />
       </Router>
-    </Div100vh>
+    </div>
   )
 }
 
