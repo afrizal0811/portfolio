160937ef35e8e644d90ce0d5d1dcec88f2605900
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-06-18 13:36:40 +0700

    add explanation text

--- src/pages/projects/help.js
+++ src/pages/projects/help.js
@@ -65,19 +65,19 @@ export const choicesList = (random) => {
       id: 0,
       response: responses,
       explain:
-        'This is web customer for car rental and created for final exam in Binar Academy Bootcamp. Build with React.Js and Bootstrap. Check this out!',
+        'This is web customer for car rental and created for final exam in Binar Academy Bootcamp. Build with React.js and Bootstrap. Check this out!',
     },
     {
       response: responses,
       id: 1,
       explain:
-        'This is crowdfunding application. Build with React.Js and Express.Js. Check this out!',
+        'This is crowdfunding application. Build with React.js, Express.js, Styled Components, and Ant Design. Check this out!',
     },
     {
       id: 2,
       response: responses,
       explain:
-        'This is credit application. Build with React Typescript and Express.Js. This is a private website, but I will show you some screenshot from the application. Check this out!',
+        'This is credit application. Build with React Typescript, Express.js, Styled Components, and Ant Design. This is a private website, but I will show you some screenshot from the application. Check this out!',
     },
   ]
 }
