d3b872ce3fb29f89a27c7a0be2aa225907074556
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-12-07 12:59:50 +0700

    edit response-explain

--- src/pages/about/help.js
+++ src/pages/about/help.js
@@ -12,6 +12,24 @@ export const choicesList = [
   {
     id: 2,
     response: 'What do you like?',
-    explain: `I’m passionate when it comes to new games, technologies and always curious to learn more creative coding, especially about web developer.`,
+    explain: `I’m passionate when it comes to new games, technologies and always curious to learn more creative coding, especially about web development.`,
+  },
+  {
+    id: 3,
+    response: 'What are your skills?',
+    explain:
+      'I am proficient in HTML, CSS, JavaScript, React.js, SQL, and Express.js. I am also learning and exploring new frameworks and tools, like ERP.',
+  },
+  {
+    id: 4,
+    response: 'What are your career goals?',
+    explain:
+      'I look forward to growing in my current role at Pangan Lestari as EDP staff and contributing to the company. I also aspire to continue learning and growing in the field of information technology.',
+  },
+  {
+    id: 5,
+    response: 'How do you approach learning new technologies?',
+    explain:
+      'I focus on hands-on practice, building projects, and participating in online communities to stay updated and get feedback from other developers. Additionally, I apply what I learn in my work at Pangan Lestari to improve processes and systems.',
   },
 ]
