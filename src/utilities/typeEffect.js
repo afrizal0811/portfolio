506479c232394425f83abb079d78dc1e61e87e56
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-12-23 13:51:17 +0700

    cleanup type effect

--- src/utilities/typeEffect.js
+++ src/utilities/typeEffect.js
@@ -1,10 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 
-const useTypingEffect = (
-  textToType,
-  interKeyStrokeDurationInMs,
-  setIsFinished
-) => {
+const useTypingEffect = (text, delay, setIsFinished) => {
   const catchFinish = typeof setIsFinished !== 'undefined' ? true : false
   const [currentPosition, setCurrentPosition] = useState(0)
   const currentPositionRef = useRef(0)
@@ -13,19 +9,19 @@ const useTypingEffect = (
     const intervalId = setInterval(() => {
       setCurrentPosition((value) => value + 1)
       currentPositionRef.current += 1
-      if (currentPositionRef.current > textToType.length) {
+      if (currentPositionRef.current > text.length) {
         clearInterval(intervalId)
         catchFinish && setIsFinished(true)
       }
-    }, interKeyStrokeDurationInMs)
+    }, delay)
     return () => {
       clearInterval(intervalId)
       currentPositionRef.current = 0
       setCurrentPosition(0)
     }
-  }, [interKeyStrokeDurationInMs, textToType, setIsFinished, catchFinish])
+  }, [delay, text, setIsFinished, catchFinish])
 
-  return textToType.substring(0, currentPosition)
+  return text.substring(0, currentPosition)
 }
 
 export default useTypingEffect
